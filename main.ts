939966e18dbb54f389a73d141191073334d52e9b
yacinebekka <40444225+yacinebekka@users.noreply.github.com>
2024-06-08 03:27:53 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,134 +1,125 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, TFile, Notice } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+export default class QuartoPlugin extends Plugin {
+    async onload() {
+        this.registerExtensions(['qmd'], 'markdown');
+        this.addCommand({
+            id: 'rename-qmd-to-md',
+            name: 'Convert QMD to MD',
+            callback: () => this.renameQmdToMd()
+        });
 
-interface MyPluginSettings {
-	mySetting: string;
+        this.addCommand({
+            id: 'convert-md-to-qmd',
+            name: 'Convert MD to QMD',
+            callback: () => this.convertMdToQmd()
+        });
+    }
+
+    async renameQmdToMd() {
+        const files = this.app.vault.getFiles();
+        let linksToUpdate = new Map();
+
+        // First collect all qmd files and prepare new paths
+        files.forEach(file => {
+            if (file.extension === 'qmd') {
+                const newPath = file.path.replace(/\.qmd$/, '.md');
+                linksToUpdate.set(file.path, newPath);
+            }
+        });
+
+        // Update content and rename files
+
+        for (const file of files) {
+            let content = await this.app.vault.read(file);
+            let modified = false;
+
+            // Update links in all files
+            linksToUpdate.forEach((newPath, oldPath) => {
+                let linkRegex = new RegExp(`\\[\\[(${this.escapeRegex(oldPath.replace(/\.qmd$/, ''))})(\\|[^\\]]+)?\\]\\]`, 'g');
+
+                if (content.match(linkRegex)) {
+                    content = content.replace(linkRegex, `[[${newPath.replace(/\.md$/, '')}$2]]`);
+                    modified = true;
+                }
+            });
+
+            if (modified) {
+                await this.app.vault.modify(file, content);
+            }
+
+            // Specifically rename qmd to md if it's one of those files
+            if (linksToUpdate.has(file.path)) {
+                await this.app.fileManager.renameFile(file, linksToUpdate.get(file.path));
+
+                // Add or adjust frontmatter to indicate file was originally qmd
+                let fileContent = await this.app.vault.read(file);
+                const frontMatterExists = fileContent.startsWith('---');
+                const newFrontMatter = `isQMD: true\n`;
+                
+                if (frontMatterExists) {
+                    fileContent = fileContent.replace(/---/, `---\n${newFrontMatter}`);
+                } else {
+                    fileContent = `---\n${newFrontMatter}---\n` + fileContent;
+                }
+                
+                await this.app.vault.modify(file, fileContent);
+
+                new Notice(`Converted ${file.basename} to .md and updated links.`);
+            }
+        }
+    }
+
+    async convertMdToQmd() {
+        const files = this.app.vault.getFiles();
+        let linksToUpdate = new Map();
+
+        // First collect all md files marked as isQMD and prepare new paths
+        for (const file of files) {
+            if (file.extension === 'md') {
+                let content = await this.app.vault.read(file);
+                if (content.includes('isQMD: true')) {
+                    const newPath = file.path.replace(/\.md$/, '.qmd');
+                    linksToUpdate.set(file.path, newPath);
+                }
+            }
+        }
+
+        // Update content and rename files
+        for (const file of files) {
+            let content = await this.app.vault.read(file);
+            let modified = false;
+
+            // Update links in all files
+            linksToUpdate.forEach((newPath, oldPath) => {
+                let linkRegex = new RegExp(`\\[\\[(${this.escapeRegex(oldPath.replace(/\.md$/, ''))})(\\|[^\\]]+)?\\]\\]`, 'g');
+                if (content.match(linkRegex)) {
+                    content = content.replace(linkRegex, `[[${newPath.replace(/\.qmd$/, '')}$2]]`);
+                    modified = true;
+                }
+            });
+
+            if (modified) {
+                await this.app.vault.modify(file, content);
+            }
+
+            // Specifically rename md to qmd if it's one of those files
+            if (linksToUpdate.has(file.path)) {
+                await this.app.fileManager.renameFile(file, linksToUpdate.get(file.path));
+                
+                // Remove the isQMD property
+                let updatedContent = await this.app.vault.read(file);
+                updatedContent = updatedContent.replace(/\n?isQMD: true\n?/, ''); // Removing the isQMD line
+                await this.app.vault.modify(file, updatedContent);
+
+                new Notice(`Converted ${file.basename} to .qmd and updated links.`);
+            }
+        }
+    }
+
+    escapeRegex(string) {
+        return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+    }
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
